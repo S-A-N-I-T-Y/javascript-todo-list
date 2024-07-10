9b33b1d36f57d6d3cb8249ec470887b292568b1f
wei3d_sanity <justinokpara19@gmail.com>
2024-07-10 15:28:43 +0100

    made changes and publishing

---
+++ index.js
@@ -0,0 +1,42 @@
+"use strict";
+const inputBox = document.getElementById("input-box");
+
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You must add a task");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  (e) => {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();  
+      saveData();
+    }
+  },
+  false
+);
+
+let saveData = () => {
+  localStorage.setItem("data", listContainer.innerHTML);
+};
+
+let showtask = () => {
+  listContainer.innerHTML = localStorage.getItem("data");
+};
+showtask();
